a260670e9503ed926c7dd8a7690745318cf3e879
femnixx <femnitality@gmail.com>
2025-11-05 18:38:18 +0700

    Finished with login and sign up page

--- src/client/auth/LoginPage.tsx
+++ src/client/auth/LoginPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import SignUp from './SignUp'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='w-screen h-screen'>
+      <div className='p-5'>
+        <h1>Login Page</h1>
+        <div className='flex flex-col gap-y-2 w-fit mt-5'>
+          <p>Email</p>
+          <input type="email" className='border'/>
+          <p>Password</p>
+          <input type="password" className='border'/>
+        <div className=' flex justify-center items-center'><button className='border px-2 cursor-pointer'>Submit</button></div>
+        <nav>Don't have an account? <Link to="/sign-up" className='underline'>Sign Up</Link></nav>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/client/auth/SignUp.tsx
+++ src/client/auth/SignUp.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div className='w-screen h-screen'>
-
+   <div className='w-screen h-screen'>
+      <div className='p-5'>
+        <h1>Login Page</h1>
+        <div className='flex flex-col gap-y-2 w-fit mt-5'>
+          <p>Username</p>
+          <input type="text" className='border'/>
+          <p>Email</p>
+          <input type="email" className='border'/>
+          <p>Password</p>
+          <input type="password" className='border'/>
+        <div className=' flex justify-center items-center'><button className='border px-2 cursor-pointer'>Submit</button></div>
+        <nav>Don't have an account? <Link to="/sign-in" className='underline'>Sign In</Link></nav>
+        </div>
+      </div>
     </div>
   )
 }
