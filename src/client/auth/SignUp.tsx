3b4adca2d8f454e8da3e8da12de249e053223daf
femnixx <femnitality@gmail.com>
2025-11-05 09:20:15 +0700

    Added the pages and a simple route for backend

---
+++ src/client/auth/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-screen h-screen'>
+
+    </div>
+  )
+}
+
+export default SignUp
---
+++ src/server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("FoodMind API Running");
+});
+
+app.listen(5000, () => console.log("Server running on port http://localhost:5000"));
